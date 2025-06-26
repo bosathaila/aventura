49ecffa427671ac7c540850edbb53060c70b39cc
bosathaila <thaila.bosa@escola.pr.gov.br>
2025-06-26 11:47:13 +0000

    26/06.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const avanca = document.querySelectorAll ('.btn-proximo');
 avanca.forEach(button =>){
     button.addEventListener ('click,function'(){
 const atual = document. querySelector ('.ativo');
-const proximoPassso = 'passo' + this.getAttribute ('data-proximo');
+const proximoPassso = 'passo'+ this.getAttribute ('data-proximo');
 
 atual. classList.remove ('ativo');
 document. getElementById (proximoPassso). classList.add('ativo');
